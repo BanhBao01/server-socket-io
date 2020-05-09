05e3dc17f9d574a586c78e70373585f885d93c1b
QuocBinh <tranaiquoc4499@gmail.com>
2020-05-09 15:56:28 +0700

    -

--- index.js
+++ index.js
@@ -9,35 +9,43 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
     console.log(`${socket.id} connection`);
+    io.emit('SOCKET_ID', socket.id)
     socket.on('CHAT_MESSAGE', (msg) => {
-        const data = JSON.stringify(msg.data)
-        const options = {
-            hostname: msg.options.hostname,
-            port: msg.options.port,
-            path: msg.options.path,
-            method: msg.options.method,
-            headers: {
-                'Content-Type': 'application/json',
-                'Content-Length': data.length
-            }
-        }
-        const req = https.request(options, (res) => {
-            console.log('STATUS: ' + res.statusCode);
-            console.log('HEADERS: ' + JSON.stringify(res.headers));
-            res.setEncoding('utf8');
-            res.on('data', (d) => {
-                io.emit('CHAT_MESSAGE', d)
-            })
-        })
-
-        req.on('error', (error) => {
-            console.error(error.message)
-        })
-
-        req.write(data)
-        req.end()
+        handleEmitSocket('CHAT_MESSAGE', msg)
+    });
+    socket.on('CHAT_MESSAGE_GROUP', (msg) => {
+        handleEmitSocket('CHAT_MESSAGE_GROUP', msg)
     });
 });
+
+function handleEmitSocket(type, msg) {
+    const data = JSON.stringify(msg.data)
+    const options = {
+        hostname: msg.options.hostname,
+        port: msg.options.port,
+        path: msg.options.path,
+        method: msg.options.method,
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': data.length
+        }
+    }
+    const req = https.request(options, (res) => {
+        console.log('STATUS: ' + res.statusCode);
+        console.log('HEADERS: ' + JSON.stringify(res.headers));
+        res.setEncoding('utf8');
+        res.on('data', (d) => {
+            io.emit(type, d)
+        })
+    })
+
+    req.on('error', (error) => {
+        console.error(error.message)
+    })
+
+    req.write(data)
+    req.end()
+}
 
 const port = process.env.PORT || 3000
 
