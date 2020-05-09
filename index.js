c5b3189858ce38f27d756984ba55a6423a584af5
QuocBinh <tranaiquoc4499@gmail.com>
2020-05-09 15:39:08 +0700

    -

--- index.js
+++ index.js
@@ -38,6 +38,8 @@ io.on('connection', (socket) => {
     });
 });
 
-http.listen(3000, () => {
-    console.log('listening on *:3000');
+const port = process.env.PORT || 300
+
+http.listen(port, () => {
+    console.log(`listening on *:${port}`);
 });
