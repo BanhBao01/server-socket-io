fdef20d7ec86444444f64ac63b57090e1f9c1f20
QuocBinh <tranaiquoc4499@gmail.com>
2020-05-09 18:50:39 +0700

    -

--- index.js
+++ index.js
@@ -19,6 +19,30 @@ io.on('connection', (socket) => {
     });
 });
 
+function startKeepAlive() {
+    setInterval(function() {
+        var options = {
+            host: 'server-socket-io0.herokuapp.com',
+            port: 80,
+            path: '/'
+        };
+        http.get(options, function(res) {
+            res.on('data', function(chunk) {
+                try {
+                    // optional logging... disable after it's working
+                    console.log("HEROKU RESPONSE: " + chunk);
+                } catch (err) {
+                    console.log(err.message);
+                }
+            });
+        }).on('error', function(err) {
+            console.log("Error: " + err.message);
+        });
+    }, 1 * 60 * 1000); // load every 20 minutes
+}
+
+startKeepAlive();
+
 function handleEmitSocket(type, msg) {
     const data = JSON.stringify(msg.data)
     const options = {
