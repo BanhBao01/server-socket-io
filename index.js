e2f25a550a3b70272c2da20813b1475d1a7083d2
QuocBinh <tranaiquoc4499@gmail.com>
2020-05-09 15:09:29 +0700

    -

---
+++ index.js
@@ -0,0 +1,45 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+const https = require('https')
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log(`${socket.id} connection`);
+    socket.on('CHAT_MESSAGE', (msg) => {
+        const data = JSON.stringify(msg.data)
+        const options = {
+            hostname: msg.options.hostname,
+            port: msg.options.port,
+            path: msg.options.path,
+            method: msg.options.method,
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': data.length
+            }
+        }
+        const req = https.request(options, (res) => {
+            console.log(`statusCode: ${res.statusCode}`)
+
+            res.on('data', (d) => {
+                io.emit('CHAT_MESSAGE', d)
+            })
+        })
+
+        req.on('error', (error) => {
+            console.error(error)
+            io.emit('CHAT_MESSAGE', error)
+        })
+
+        req.end()
+    });
+});
+
+const port = process.env.PORT || 3000
+
+http.listen(port, () => {
+    console.log(`listening on *:${port}`);
+});
